907f3d1d10382c03ab18fba0a75d1ee28ee5c138
Eric Swotinsky and Panatda Inthala <eric@>
2018-02-12 13:32:35 -0800

    setup project structure.

---
+++ QueenAttack.Tests/ModelTests/QueenAttackTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QueenAttack;
+
+namespace QueenAttack.Tests
+{
+  [TestClass]
+  public class QueenAttackTest
+  {
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
